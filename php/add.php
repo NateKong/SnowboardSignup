5915f1642570167fad4d5f283ae29e58556ffd7f
natekong <natewkong@gmail.com>
2017-06-16 23:40:34 -0700

    added post

--- php/add.php
+++ php/add.php
@@ -3,6 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "nate";
 $dbname = "epic";
+$data = '';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,59 +12,42 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
+//echo $_POST['name'] . " " . $_POST['date'] . " " . $_POST['pass'];
+//add to table
+$post = "INSERT INTO pass VALUES ('" . $_POST['name'] . "','" . $_POST['date'] . "','" . $_POST['pass'] . "')";
 
+if($conn->query($post) === TRUE){
+  //Query table
+  
+  $querySql = "SELECT * FROM pass";
+  $result = $conn->query($querySql);
 
-
-
-
-
-
-
-
-$querySql = "SELECT * FROM pass";
-$result = $conn->query($querySql);
-$data = '<li class="collection-item">
-            <div class="row" >
-              <div class="col s1 switch">
-                <label>
-                	<input id="checkbox-delete" type="checkbox">
-                	<span class="lever"></span>
-                </label>
-              </div>
-              <div class="col s4 "><strong>Name</strong></div>
-              <div class="col s3 "><strong>Birthday</strong></div>
-              <div class="col s4 "><strong>Pass</strong></div>
-            </div>
-         </li>
-          <script>
-           	$("input#checkbox-delete").change(function() {
-				$("button").toggle();
-			});
-		  </script>';
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        
-        $data .=
-	        '<li class="collection-item">
-               <div class="row">
-        		 <div class="col s1">
-            		<button class="btn waves-effect red" style="display: none">delete</button>
-            	 </div>
-            	 <div id="name1" class="col s4">' . $row["name"] . '</div>
-            	 <div class="col s3">' . date( "m/d/Y", strtotime ($row["date"])) . '</div>
-            	 <div class="col s4">' . $row["pass"] . '</div>
-               </div>
-             </li>';
-            
-    }
-} else {
-    echo "0 results";
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          
+          $data .=
+            '<li class="collection-item">
+                 <div class="row">
+               <div class="col s1">
+                  <button class="btn waves-effect red" style="display: none">delete</button>
+                 </div>
+                 <div id="name1" class="col s4">' . $row["name"] . '</div>
+                 <div class="col s3">' . date( "m/d/Y", strtotime ($row["date"])) . '</div>
+                 <div class="col s4">' . $row["pass"] . '</div>
+                 </div>
+               </li>';
+              
+      }
+  } else {
+      echo "0 results";
+  }
+  
+}else{
+  echo "Error: " . $post . " " . $conn->error;
 }
 
 $conn->close();
 
 echo $data;
-
 ?>
